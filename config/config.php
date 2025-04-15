d6017d92150cfdf1d9e7efd9fb4bb45dfb9ddd23
henryque <henryquenonatosilva@gmail.com>
2025-04-15 09:37:07 -0300

    config views

--- config/config.php
+++ config/config.php
@@ -1,3 +1,12 @@
 <?php 
 
-define('BASE_URL', 'http://localhost/guloseimas_do_limpoapp/public/');
+define('BASE_URL', 'http://localhost/guloseimas_do_olimpoapp/public/');
+
+spl_autoload_register(function ($class) {
+    if (file_exists('../app/controllers/' . $class . '.php')) {
+        require_once '../app/controllers/' . $class . '.php';
+    }
+    if (file_exists('../rotas/' . $class . '.php')) {
+        require_once '../rotas/' . $class . '.php';
+    }
+});
