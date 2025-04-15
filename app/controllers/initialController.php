d60a921f09720ed60a6e127ec8a3c8219ad368ce
henryque <henryquenonatosilva@gmail.com>
2025-04-15 09:36:20 -0300

    controller initial

---
+++ app/controllers/initialController.php
@@ -0,0 +1,13 @@
+<?php
+
+class initialController extends Controller
+{
+    public function index()
+    {
+
+        $dados = array();
+        $dados['titulo'] = 'Guloseimas do olimpo - Tela inicial';
+        
+        $this->carregarViews('initial', $dados);
+    }
+}
