5a244c32f58f0aa2c0c99386d0903548cd788ca8
henryque <henryquenonatosilva@gmail.com>
2025-04-29 11:50:16 -0300

    menu controller

--- app/controllers/menuController.php
+++ app/controllers/menuController.php
@@ -4,25 +4,55 @@ class menuController extends Controller
 {
     public function index()
     {
-        // Verifica se o token está presente
-        if (!isset($_SESSION['token'])) {
-            header("Location: " . BASE_URL . "index.php?url=login");
-            exit;
+
+        // if (!isset($_SESSION['token'])) {
+        //     header("Location: " . BASE_URL . "index.php?url=login");
+        //     exit;
+        // }
+
+        // $dadosToken = TokenHelper::validar($_SESSION['token']);
+
+        // if (!$dadosToken) {
+        //     session_destroy();
+        //     unset($_SESSION['token']);
+        //     header("Location: " . BASE_URL . "index.php?url=login");
+        //     exit;
+        // }
+
+        // buscar cliente na api
+        $url = BASE_API  . "cliente/" . $dadosToken['id'];
+
+        // Reconhecimento da chave(Inicializa uma sessão cURL)
+        $ch = curl_init($url);
+
+        // Definir que o conteudo venha com a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'authorization: Bearer ' . $_SESSION['token']
+        ]);
+        
+        // recebe os dados dessa solicitação
+        $response = curl_exec($ch);
+
+        // Obtém o código HTTP da resposta (200, 400, 401)
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        // Encerra a sessão Curl
+        curl_close($ch);
+
+        if ($statusCode != 200) {
+            echo "Erro ao buscar cliente na API.\n
+            Código HTTP: $statusCode";
         }
 
-        // Valida o token
-        $dadosToken = TokenHelper::validar($_SESSION['token']);
+        // separa os dados em 'campos'
+        $cliente = json_decode($response, true);
 
-        if (!$dadosToken) {
-            session_destroy();
-            unset($_SESSION['token']);
-            header("Location: " . BASE_URL . "index.php?url=login");
-            exit;
-        }
-
-        // Se passou pela validação, carrega a view
         $dados = array();
-        $dados['titulo'] = 'Guloseimas do olimpo - Menu';
+        $dados['titulo'] = 'kiOficina - Menu';
+
+        $dados['nome_cliente'] = $cliente['nome_cliente'] ?? 'CLiente';
 
         $this->carregarViews('menu', $dados);
     }
