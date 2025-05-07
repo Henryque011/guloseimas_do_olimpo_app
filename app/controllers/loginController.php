c86ada3fc69b0f7917b1871c57ea3eed21ec3461
henryque <henryquenonatosilva@gmail.com>
2025-05-07 09:21:39 -0300

    controller login para logout

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -70,4 +70,39 @@ class loginController extends Controller
         header("Location: " . BASE_URL . "index.php?url=login");
         exit;
     }
+
+    public function sair()
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $_SESSION = [];
+
+        if (isset($_COOKIE['email'])) {
+            setcookie('email', '', time() - 3600, "/");
+        }
+
+        if (isset($_COOKIE['senha'])) {
+            setcookie('senha', '', time() - 3600, "/");
+        }
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        session_destroy();
+
+        header("Location: " . BASE_URL . "index.php?url=login");
+        exit;
+    }
 }
