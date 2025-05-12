8b8f6670b404213bac77b1ac785eef59f76bb281
henryque <henryquenonatosilva@gmail.com>
2025-05-12 11:40:29 -0300

    pagina esqueci a senha

---
+++ app/views/esqueci_senha.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php
+require_once('template/head.php')
+?>
+
+<body>
+    <?php
+    require_once('template/header.php')
+    ?>
+    <section class="esqueci_senha">
+        <article class="site">
+            <h2>Recuperar senha</h2>
+            <div class="container">
+                <h3>Informe um email para recuperação</h3>
+
+            </div>
+        </article>
+    </section>
+</body>
+
+</html>
