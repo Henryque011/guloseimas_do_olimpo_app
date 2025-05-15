4981f9a2754a9b8f9d43cd11eed400e3f8beeaca
henryque <henryquenonatosilva@gmail.com>
2025-05-15 11:32:36 -0300

    view esqueci-senha

--- app/views/esqueci_senha.php
+++ app/views/esqueci_senha.php
@@ -12,8 +12,16 @@ require_once('template/head.php')
         <article class="site">
             <h2>Recuperar senha</h2>
             <div class="container">
+                
+                <?php if (!empty($_SESSION['flash']) && is_array($_SESSION['flash'])): ?>
+                    <div class="alert <?= $_SESSION['flash']['tipo'] ?>">
+                        <?= $_SESSION['flash']['mensagem'] ?>
+                    </div>
+                    <?php unset($_SESSION['flash']); ?>
+                <?php endif; ?>
+
                 <h3>Informe um email para recuperação</h3>
-                <form action="">
+                <form action="<?php echo BASE_URL; ?>index.php?url=login/enviarRecuperacao" method="POST">
                     <input type="email" name="email" id="email" placeholder="email" required>
                     <hr>
                     <input type="submit" value="Enviar Link" class="btn-link">
