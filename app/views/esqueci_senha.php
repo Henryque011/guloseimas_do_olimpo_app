01046313fce5482a84c95847764f65b3b249b763
henryque <henryquenonatosilva@gmail.com>
2025-05-15 09:58:23 -0300

    button voltar

--- app/views/esqueci_senha.php
+++ app/views/esqueci_senha.php
@@ -15,10 +15,15 @@ require_once('template/head.php')
                 <h3>Informe um email para recuperação</h3>
                 <form action="">
                     <input type="email" name="email" id="email" placeholder="email" required>
+                    <hr>
                     <input type="submit" value="Enviar Link" class="btn-link">
                 </form>
             </div>
+
         </article>
+        <div class="button_voltar">
+            <a href="<?php echo BASE_URL; ?>index.php?url=initial"><i class="fa-solid fa-backward"></i>Voltar</a>
+        </div>
     </section>
 
 </body>
