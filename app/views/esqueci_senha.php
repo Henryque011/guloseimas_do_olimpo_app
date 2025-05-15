600999bc7127877c8012c07fb3d39138411b7daa
henryque <henryquenonatosilva@gmail.com>
2025-05-15 09:23:42 -0300

    view pagina esquecia senha

--- app/views/esqueci_senha.php
+++ app/views/esqueci_senha.php
@@ -14,15 +14,13 @@ require_once('template/head.php')
             <div class="container">
                 <h3>Informe um email para recuperação</h3>
                 <form action="">
-                    <label for="email">E-mail:</label>
-                    <input type="email" name="email" id="email" required>
-
+                    <input type="email" name="email" id="email" placeholder="email" required>
                     <input type="submit" value="Enviar Link" class="btn-link">
                 </form>
-
             </div>
         </article>
     </section>
+
 </body>
 
 </html>
