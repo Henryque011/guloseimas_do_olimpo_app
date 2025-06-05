44af132c7481397d38cabd1df1695948d569e471
henryque <henryquenonatosilva@gmail.com>
2025-06-05 11:38:07 -0300

    :✨:fecth: Listar produtos na view

--- app/views/produtos.php
+++ app/views/produtos.php
@@ -1,3 +1,9 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <?php
@@ -16,7 +22,31 @@ require_once('template/head.php')
                 <p>Preço: R$ <span id="preco-atual">0</span></p>
                 <h3>Filtrar por categoria</h3>
             </div>
+
+            <div class="produtos">
+                
+                <?php if (isset($produtos) && is_array($produtos)): ?>
+                    <?php foreach ($produtos as $produto): ?>
+                        <div class="card-produto">
+                            <h4><?= htmlspecialchars($produto['nome_produto']) ?></h4>
+                            <p><?= htmlspecialchars($produto['descricao_produto']) ?></p> 
+                            <p>Preço: R$ <?= number_format($produto['preco_produto'], 2, ',', '.') ?></p>
+                            <p>Categoria: <?= htmlspecialchars($produto['id_categoria']) ?></p>
+
+                            <?php if (!empty($produto['foto_produto'])): ?>
+                                <img src="<?= BASE_URL . 'public/assets/' . $produto['foto_produto'] ?>"
+                                    alt="<?= htmlspecialchars($produto['alt_foto_produto']) ?>"
+                                    width="150">
+                            <?php endif; ?>
+                        </div>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <p>Nenhum produto encontrado.</p>
+                <?php endif; ?>
+            </div>
         </article>
+
+        
     </section>
 
     <script>
