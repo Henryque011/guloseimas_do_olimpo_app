4d4e6ab2d69eca4cadb39c1ff86645f7a4e6a0aa
henryque <henryquenonatosilva@gmail.com>
2025-06-05 11:37:20 -0300

    :🌟:fecth: ProdutosController - Listar Produtos.

--- app/controllers/ProdutosController.php
+++ app/controllers/ProdutosController.php
@@ -1,11 +1,58 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-class ProdutosController extends Controller{
+class ProdutosController extends Controller
+{
     public function index()
     {
-        $dados = array();
-        $dados['titulo'] = 'Guloseimas do olimpo - Produtos';
 
+        if (!isset($_SESSION['token'])) {
+            header("location: " . BASE_URL . "index.php?url=login");
+            exit;
+        }
+
+        $dadoToken = TokenHelper::validar($_SESSION['token']);
+
+        if (!$dadoToken) {
+            session_destroy();
+            unset($_SESSION['token']);
+            header("location: " . BASE_URL . "index.php?url=login");
+            exit;
+        }
+
+
+        // Buscar produtos na API (ajuste o endpoint conforme necessário)
+        $url = BASE_API . "listarProdutos";
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'AUTHORIZATION: Bearer ' . $_SESSION['token']
+        ]);
+
+        $response = curl_exec($ch);
+
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+
+        if ($statusCode != 200) {
+            echo "Erro ao buscar os produtos na API.\n
+        Código HTTP: $statusCode";
+            exit;
+        }
+
+        $produtos = json_decode($response, true);
+
+        $dados = array();
+        $dados['titulo'] = 'kiOficina - listar produtos';
+        $dados['produtos'] = $produtos;
+
+        // Carregar a view apropriada para produtos
         $this->carregarViews('produtos', $dados);
     }
 }
