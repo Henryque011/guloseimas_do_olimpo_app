d72f2b1a773a542e21cc709c6c1d06556e34e242
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-06-26 07:12:22 -0300

    view

--- app/controllers/ReservasController.php
+++ app/controllers/ReservasController.php
@@ -2,46 +2,34 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-
 class ReservasController extends Controller
 {
     public function index()
     {
-        // Verifica se o usuário está logado
         if (!isset($_SESSION['token'])) {
             header("location: " . BASE_URL . "index.php?url=login");
             exit;
         }
 
         $dadoToken = TokenHelper::validar($_SESSION['token']);
-        if (!$dadoToken || !isset($dadoToken['id_cliente'])) {
+        if (!$dadoToken || empty($dadoToken['id'])) {
             session_destroy();
             unset($_SESSION['token']);
             header("location: " . BASE_URL . "index.php?url=login");
             exit;
         }
 
-        $id_cliente = $dadoToken['id_cliente'];
-
-
-        if (!$id_cliente) {
-            echo "ID do cliente não encontrado.";
-            exit;
-        }
+        $id_cliente = $dadoToken['id'];
 
         $urlReservas = BASE_API . 'getReservasPorCliente?id_cliente=' . $id_cliente;
 
         $ch = curl_init($urlReservas);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'AUTHORIZATION: Bearer ' . $_SESSION['token']
+            'Authorization: Bearer ' . $_SESSION['token']
         ]);
         $response = curl_exec($ch);
         curl_close($ch);
-        echo "<pre>";
-        print_r($_SESSION);
-        echo "</pre>";
-        exit;
 
         $reservas = json_decode($response, true);
 
@@ -49,6 +37,6 @@ class ReservasController extends Controller
         $dados['titulo'] = 'Minhas Reservas';
         $dados['reservas'] = $reservas;
 
-        $this->carregarViews('reserva', $dados);
+        $this->carregarViews('reservas', $dados);
     }
 }
